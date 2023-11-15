160154d8cd22bca084bec4f78c454d01ae8ebe36
Robby Sarvis <rjs.app.dev@gmail.com>
2023-11-14 21:39:49 -0600

    Initial Commit

---
+++ DaysUntil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DaysUntil
+//
+//  Created by Robert J. Sarvis Jr on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DaysUntil/DaysUntilApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysUntilApp.swift
+//  DaysUntil
+//
+//  Created by Robert J. Sarvis Jr on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DaysUntilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
